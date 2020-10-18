d9aebfa0f41c57d4c081056b4ad5f0e3cc093e79
Gréta Nováky <greta.novaky@stud.uni-corvinus.hu>
2020-10-18 19:56:38 +0200

    3fel

--- 6heti/Form1.cs
+++ 6heti/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6heti.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,22 @@ namespace _6heti
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
         }
+
+        void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
+        }
+          
     }
 }
